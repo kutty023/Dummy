428e79e54c513c62bc5edeb30970c6fcb031f1d6
kutty023 <arthiaaru23@gmail.com>
2024-04-05 13:17:40 +0530

    Connected db to server

--- BackendServer/server.js
+++ BackendServer/server.js
@@ -2,29 +2,26 @@ const express = require('express');
 const app = express();
 const port = 3000;
 require('dotenv').config()
+const mongoose = require('mongoose')
+const mongodburi = process.env.MONGODB_URI
 
 // Get request
 app.get('/ping', (req, res) => {
   res.send("Pong")
 });
 
+// connecting mongoDb
+mongoose.connect(mongodburi)
 
+// Define a route to check the MongoDB connection status
+app.get('/mongoDbConnection', (req, res) => {
+  if(mongoose.connection.readyState === 1){
+    res.status(200).json('MongoDb connection is successful')
+  }else{
+    res.status(400).json('Error connecting to mongodb')
+  }
+});
 
-// const mongoose = require('mongoose');
-// const mongodburi = process.env.MONGODB_URI;
-
-// // Connect to MongoDB
-// mongoose.connect(mongodburi, 
-//   { useNewUrlParser: true, useUnifiedTopology: true });
-
-// // Define a route to check the MongoDB connection status
-// app.get('/mongo', (req, res) => {
-//   if (mongoose.connection.readyState === 1) {
-//     res.status(200).send('MongoDB connection is active.');
-//   } else {
-//     res.status(500).send('MongoDB connection is not active.');
-//   }
-// });
 
 
 if (require.main === module) {
